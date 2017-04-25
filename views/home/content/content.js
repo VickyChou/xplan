5aa2fd0216736c2070edef316ae5ce06b9dd0bc0
vicky <vickyyoumay@hotmail.com>
2017-04-25 16:12:05 +0800

    version02,add day, backtopreday,tonextday

--- views/home/content/content.js
+++ views/home/content/content.js
@@ -19,107 +19,131 @@ angular.module('xPlan')
             }
         }
     })
-    .directive('addNewDay',function ($compile) {
-        return{
-            restrict:'A',
-            link:function (scope,el,attr) {
-                var clickNumber=0;//定义初始点击次数，为0
-                var routeData=localStorage.getItem('routeData');//获取第一天的值
-                routeData=JSON.parse(routeData);
-                el[0].addEventListener('click',function () {
-                    clickNumber=clickNumber+1;//表示点击次数
-                    var dayNumber=Number(routeData[0][0])+clickNumber;//第一天的值加1，成为第二天
-                    routeData[0].push(dayNumber);//把第二天的值存入数组
-                    /*日期增加一天开始*/
-                    function addDate(date,days){
-                        var d=new Date(date);
-                        d.setDate(d.getDate()+days);
-                        var month=d.getMonth()+1;
-                        var day = d.getDate();
-                        if(month<10){
-                            month = "0"+month;
-                        }
-                        if(day<10){
-                            day = "0"+day;
-                        }
-                        var val = d.getFullYear()+"-"+month+"-"+day;
-                        return val;
-                    }
-                    var date=new Date();
-                    var firstDay=date.getFullYear()+'-'+(date.getMonth()+1)+'-'+date.getDate();
-                    firstDay=addDate(firstDay,clickNumber);
-                    /*日期增加一天结束*/
-                    routeData[1].push(firstDay);//存储新的日期
-                    localStorage.routeData=JSON.stringify(routeData); //存储新增一天的所有数据
-                    document.getElementById('day-num').innerText=dayNumber;
-                    document.getElementById('first-day').innerText=firstDay;
-                    var dropDownIcon=document.getElementById('drop-down');
-                    dropDownIcon.removeAttribute('class');
-                    var sight=document.getElementsByClassName('sight-container');
-                    var length=sight.length;
-                    for(var i=0;i<length;i++){
-                        sight[i].remove();
-                    }
-                })
-            }
-        }
-    })
-    .directive('backToPreDay',function () {
-        return{
-            restrict:'A',
-            link:function (scope,el,attr) {
-                el[0].addEventListener('click',function () {
-                    var routeData=localStorage.getItem('routeData');//获取路书数据
-                    routeData=JSON.parse(routeData);
-                    var dayNumEle=document.getElementById('day-num');
-                    var dayNumText=dayNumEle.innerText;
-                    var dayNum=Number(dayNumText);
-                    if(dayNum===1) return false;
-                    else {
-                        var index=routeData[0].indexOf(dayNum);
-                        dayNumEle.innerText=routeData[0][index-1];
-                        document.getElementById('first-day').innerText=routeData[1][index-1];
-                        var sightEle='<div class="sight-container" show-icon>' +
-                            '<div class="sight-img"><img src="" alt="" name="locationImage" ng-model="locationImage"></div>' +
-                            '<div class="sight-title"><a href="" ng-bind="locationName"></a>' +
-                            '<p id="edit-icons" class="hide">' +
-                            '<a href=""><i class="fa fa-edit"></i></a>&nbsp' +
-                            '<a href=""><i class="fa fa-trash-o"></i></a>' +
-                            '</p>' +
-                            '</div>' +
-                            '</div>'
-                    }
-                })
-            }
-        }
-    })
-    .controller('contentCtrl',function ($scope) {
+    .controller('contentCtrl',function ($scope,$uibModal) {
         /*规划页面初始化开始*/
+
+        /*获取当天日期，设置为第一天*/
         $scope.dayNumber=1;
-        $scope.showDrapdownIcon=false;
         var date=new Date();
         $scope.firstTravelDate=date.getFullYear()+'-'+(date.getMonth()+1)+'-'+date.getDate();
+        $scope.showDrapdownIcon=false;
         $scope.status = {
             isopen: false
         };
-        $scope.locationName=sessionStorage.getItem('cityMessage');
-        /*规划页面初始化结束*/
+        $scope.showsights=true;
 
-        /*获取天数及景点数据并存储--开始*/
-        var locationName=$scope.locationName;
-        var imgUrl=routeForm.locationImage.src;
+        /*获取景点数据并存储*/
+        var locationNames=sessionStorage.getItem('cityMessage');//获取地图定位的城市名称及图片
+        var currentDayLocations=[locationNames];
+        var imgUrls=['/xplan/images/city_image.svg'];
+        var currentLocationNameImgUrl=[imgUrls];
+        var locations=[currentLocationNameImgUrl,currentDayLocations];
+        $scope.imgUrls=imgUrls;
+        $scope.locationNames=currentDayLocations;
+
+        /*绑定景点之间的间距*/
         $scope.spaceBetweenTwoSights='';
-        var distance=$scope.spaceBetweenTwoSights;
+        var distance=[$scope.spaceBetweenTwoSights];
         $scope.drivingTimeBetweenTwoSights='';
-        var drivingTime=$scope.drivingTimeBetweenTwoSights;
+        var drivingTime=[$scope.drivingTimeBetweenTwoSights];
+        var distanceArray=[distance];
+        var drivingTimeArray=[drivingTime];
+
+        /*存储新增的天数和日期*/
         var dayNum=$scope.dayNumber;
         var firstDay=$scope.firstTravelDate;
         var dayNumArray=[dayNum];
         var firstDayArray=[firstDay];
-        var locationNameArray=[locationName];
-        var imgUrlArray=[imgUrl];
-        var distanceArray=[distance];
-        var drivingTimeArray=[drivingTime];
-        localStorage.routeData=JSON.stringify([dayNumArray,firstDayArray,locationNameArray,imgUrlArray,distanceArray,drivingTimeArray]);
-        /*获取天数及景点数据并存储--结束*/
+
+        /*存储路书中所有数据*/
+        var routeData=[dayNumArray,firstDayArray,locations,distanceArray,drivingTimeArray];
+        /*localStorage.routeData=JSON.stringify(routeData);*/
+
+        /*规划页面初始化结束*/
+
+        /*新增一天行程*/
+        $scope.addNewDay=function () {
+            firstDay=addDate(firstDay,1);
+            $scope.firstTravelDate=firstDay;
+            dayNum=dayNum+1;
+            $scope.dayNumber=dayNum;
+            $scope.showDrapdownIcon=true;
+            var index=dayNumArray.indexOf(dayNum);
+            $scope.imgUrls=[currentLocationNameImgUrl[index]];
+            $scope.locationNames=[currentDayLocations[index]];
+            $scope.showsights=false;
+            dayNumArray.push(dayNum);
+            $scope.dayNumArray=dayNumArray;
+            firstDayArray.push(firstDay);
+        };
+
+        /*返回上一天行程*/
+        $scope.backToPreDay=function () {
+            var currentDayNum=$scope.dayNumber;
+            if(currentDayNum===1) return false;
+            var currentDate=$scope.firstTravelDate;
+            var preDayDate=addDate(currentDate,-1);
+            $scope.firstTravelDate=preDayDate;
+            $scope.dayNumber=currentDayNum-1;
+            if((currentDayNum-1)===1){
+                var index=dayNumArray.indexOf(currentDayNum-1);
+                $scope.imgUrls=imgUrls;
+                $scope.locationNames=currentDayLocations;
+                $scope.showsights=true;
+            }
+        };
+
+        /*到下一天*/
+        $scope.toNextDay=function () {
+            var currentDayNum=$scope.dayNumber;
+            var maxDay=dayNumArray.length;
+            if(maxDay===1) return false;
+            else if(currentDayNum===maxDay) return false;
+            var currentDate=$scope.firstTravelDate;
+            var preDayDate=addDate(currentDate,1);
+            $scope.firstTravelDate=preDayDate;
+            $scope.dayNumber=currentDayNum+1;
+            $scope.showsights=false;
+        };
+
+        /*删除当前这一天跳出提示框*/
+        $scope.deleteCurrentDay=function () {
+            var currentDayNum=$scope.dayNumber;
+            if(currentDayNum===1) return false;
+            else {
+                $uibModal.open({
+                    animation: true,
+                    keyboard: true,
+                    templateUrl: 'deleteDayModel.html',
+                    controller: 'deleteCurrentDayCtrl',
+                    scope: $scope
+                })
+            }
+        };
+    })
+    .controller('deleteCurrentDayCtrl',function ($scope,$uibModalInstance) {
+        /*取消删除当前天*/
+        $scope.cancel=function () {
+            $uibModalInstance.dismiss('cancel');
+        };
+
+        /*确认删除当前天*/
+        $scope.ok=function () {
+        }
     });
+
+//日期增加、减少一天
+function addDate(date,days){
+    var d=new Date(date);
+    d.setDate(d.getDate()+days);
+    var month=d.getMonth()+1;
+    var day = d.getDate();
+    if(month<10){
+        month = "0"+month;
+    }
+    if(day<10){
+        day = "0"+day;
+    }
+    return d.getFullYear()+"-"+month+"-"+day;
+}
+
